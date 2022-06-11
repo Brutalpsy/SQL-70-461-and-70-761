30ada395ed84289d3baae3c4c1936fdbe755c3d6
Brutalpsy <brutalpsy89@gmail.com>
2022-06-12 01:19:54 +0200

    joined three tables and done queries

--- Section11 - Adding a second table/create department table and using derived tables and dynamic creation of the table.sql
+++ Section11 - Adding a second table/create department table and using derived tables and dynamic creation of the table.sql
@@ -18,3 +18,38 @@ FROM Employee
 
 SELECT * 
 FROM Department
+
+
+-- Connect all three tables
+SELECT *
+FROM Department
+LEFT JOIN Employee
+ON Department.Department = Employee.Department
+LEFT JOIN EmployeeTransaction
+ON Employee.EmployeeNumber = EmployeeTransaction.EmployeeNumber
+
+-- Get amount per department
+SELECT Department.Department, Sum(Amount) AS SumOfAmount
+FROM Department
+LEFT JOIN Employee
+ON Department.Department = Employee.Department
+LEFT JOIN EmployeeTransaction
+ON Employee.EmployeeNumber = EmployeeTransaction.EmployeeNumber
+GROUP BY Department.Department
+
+-- Get amount per department head
+SELECT D.DepartmentHead, Sum(Amount) AS SumOfAmount
+FROM Department AS D
+LEFT JOIN Employee AS E
+ON D.Department = E.Department
+LEFT JOIN EmployeeTransaction AS ET
+ON E.EmployeeNumber = ET.EmployeeNumber
+GROUP BY D.DepartmentHead
+ORDER BY D.DepartmentHead
+
+
+INSERT INTO Department(Department,DepartmentHead)
+VALUES('Accounts','James')
+
+SELECT * 
+FROM EmployeeTransaction
