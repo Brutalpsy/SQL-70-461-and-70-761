9a55e3213a8c01e2def9bcc1ab8033a2c3342a11
psy <brutalpsy89@gmail.com>
2022-07-12 21:08:36 +0200

    create indexed views

--- Section 16 - Views/altering and dropping views.sql
+++ Section 16 - Views/altering and dropping views.sql
@@ -7,15 +7,19 @@ IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = 'ViewByDepar
 	DROP VIEW [dbo].[ViewByDepartment]
 GO
 -- YOU CAN USE ALTER TO CHANGE A VIEW
-CREATE VIEW [dbo].[ViewByDepartment] AS
+CREATE VIEW [dbo].[ViewByDepartment] WITH SCHEMABINDING AS -- ADDED WITH SCHEMABINDING
 SELECT D.Department, ET.EmployeeNumber, ET.DateOfTransaction, ET.Amount AS TotalAmount
-FROM Department AS D
-INNER JOIN Employee AS E
+FROM dbo.Department AS D
+INNER JOIN dbo.Employee AS E -- CHANGED LEFT TO INNER
 ON D.Department = E.Department
-LEFT JOIN EmployeeTransaction AS ET
+INNER JOIN dbo.EmployeeTransaction AS ET -- CHANGED LEFT TO INNER
 ON E.EmployeeNumber = ET.EmployeeNumber
 WHERE ET.EmployeeNumber BETWEEN 120 AND 139
 
 GO
+--CREATE INDEX ON VIEW
+CREATE UNIQUE CLUSTERED INDEX inx_ViewByDepartment ON [dbo].[ViewByDepartment] (EmployeeNumber,Department, DateOfTransaction)
+
+BEGIN TRAN
 
 
