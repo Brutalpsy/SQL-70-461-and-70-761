8d6afafe4de28564ceb689b6d1b4595dfa16afb8
Brutalpsy <brutalpsy89@gmail.com>
2022-09-04 01:19:02 +0200

    added stored procedures

---
+++ Section 21 - Create and alter stored procedures/Create procedure.sql
@@ -0,0 +1,22 @@
+--CREATE SIMPLE PROCEDURE
+--IF EXISTS(SELECT * FROM sys.procedures WHERE NAME= 'NameEmployees')
+IF (SELECT OBJECT_ID('NameEmployees','P')) IS NOT NULL
+DROP PROC NameEmployees
+GO
+CREATE PROCEDURE NameEmployees(@number int) AS 
+BEGIN
+    IF EXISTS(SELECT * FROM Employee WHERE EmployeeNumber = @number)
+	BEGIN -- ISN'T REQUIRED SINCE ONLY THE FIRST STATETMENT AFTER EXIST WILL RUN,BUT ITS MORE READABLE AND YOU CAN NOW PLACE SOME MORE STATEMENTS
+		SELECT EmployeeFirstName,EmployeeLastName, EmployeeNumber 
+		FROM Employee
+		WHERE EmployeeNumber = @number
+	END
+END
+
+-- THREE WAYS OF EXECUTING A PROCEDURE
+EXECUTE NameEmployees 4
+EXEC NameEmployees 223
+GO -- REQUIRED IF WITH ONLY A NAME
+NameEmployees 333
+
+
