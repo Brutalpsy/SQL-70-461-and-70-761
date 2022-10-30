da370658e0c218d8131e3a5a0418b81e58220656
Brutalpsy <brutalpsy89@gmail.com>
2022-10-30 12:15:00 +0100

    create XML variable and XML field

---
+++ Section 39 - Query and Manage XML data/Creating XML variable and XML field.sql
@@ -0,0 +1,27 @@
+ DECLARE @X XML
+ SET @X = '<Shopping ShopperName="Danijel" Weather="awesome">
+<ShoppingTrip ShoppingTripID="L1">
+    <Item Cost="5">Bananas</Item>
+    <Item Cost="4">Apples</Item>
+    <Item Cost="3">Cherries</Item>
+</ShoppingTrip>
+<ShoppingTrip ShoppingTripID="L2">
+    <Item>Emeralds</Item>
+    <Item>Diamonds</Item>
+    <Item>Furniture</Item>
+</ShoppingTrip>
+</Shopping>'
+
+SELECT @X AS XmlOutput
+
+ALTER TABLE Employee
+ADD XmlOutput XML NULL
+
+
+
+UPDATE Employee
+SET XmlOutput = @X
+WHERE EmployeeNumber = 200
+
+SELECT * FROM Employee WHERE EmployeeNumber = 200
+
