adecde2299d588dc716909832b688888ae1eba4e
psy <brutalpsy89@gmail.com>
2022-10-31 09:47:35 +0100

    XQuery method using for and return

---
+++ Section 40 - Shredding XML/XQuery method using for and return.sql
@@ -0,0 +1,54 @@
+ DECLARE @x XML
+ SET @x = '<Shopping ShopperName="Danijel" Weather="awesome">
+<ShoppingTrip ShoppingTripID="L1">
+    <Item Cost="5">Bananas</Item>
+    <Item Cost="4">Apples</Item>
+    <Item Cost="3">Cherries</Item>
+</ShoppingTrip>
+<ShoppingTrip ShoppingTripID="L2">
+    <Item>Emeralds</Item>
+    <Item>Diamonds</Item>
+    <Item>Furniture</Item>
+</ShoppingTrip>
+</Shopping>'
+
+
+-- GET ME ALL SHOPPING ITEMS
+SELECT @x.query('for $ValueRetrieved in /Shopping/ShoppingTrip/Item 
+			     return $ValueRetrieved')
+
+-- Give me only the values inside the <Item />
+SELECT @x.query('for $ValueRetrieved in /Shopping/ShoppingTrip/Item 
+				return string($ValueRetrieved)')
+
+-- Retrun with semicolumn between each one
+SELECT @x.query('for $ValueRetrieved in /Shopping/ShoppingTrip/Item 
+				return concat(string($ValueRetrieved), ";")')
+
+-- Give me the items only from first shopping trip 
+SELECT @x.query('for $ValueRetrieved in /Shopping/ShoppingTrip[1]/Item 
+				return concat(string($ValueRetrieved), ";")')
+
+
+
+--$ - variable
+
+ 
+ -- XQuery order of execution 
+--F - for
+--L - let
+--W - where 
+--O - order by
+--R - return
+
+
+
+
+
+--SELECT, ORDER OF EXECUTION
+--S - SELECT
+--F - FROM 
+--W - WHERE
+--G - GROUP BY
+--H - HAVING 
+--O - ORDER BY
