1ed430dd765136e0b803434e0fb729cc6fb175fc
Brutalpsy <brutalpsy89@gmail.com>
2022-11-05 21:02:04 +0100

    Creating temporal tables

---
+++ Section 43 -Temporal Tables/Creating temporal tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE [dbo].[EmployeeTemporal](
+	[EmployeeNumber] [int] NOT NULL PRIMARY KEY CLUSTERED,
+	[EmployeeFirstName] [nvarchar](50) NOT NULL,
+	[EmployeeMiddleName] [nvarchar](50) NULL,
+	[EmployeeLastName] [nvarchar](50) NOT NULL,
+	[EmployeeGovermentId] [char](10) NULL,
+	[DateOfBirth] [date] NOT NULL,
+	[Department] [varchar](20) NULL,
+	ValidFrom DateTime2(2) GENERATED ALWAYS AS ROW START ,
+	ValidTo Datetime2(2) GENERATED ALWAYS AS ROW END,
+	PERIOD FOR SYSTEM_TIME (ValidFrom,ValidTo))
+	WITH (SYSTEM_VERSIONING = ON)
+
+
+
+	INSERT INTO [dbo].[EmployeeTemporal]
+           ([EmployeeNumber]
+           ,[EmployeeFirstName]
+           ,[EmployeeMiddleName]
+           ,[EmployeeLastName]
+           ,[EmployeeGovermentId]
+           ,[DateOfBirth]
+           ,[Department])
+	VALUES
+(123,'Jane',NULL,'Zwilling','AB123456G', '1985-01-01','Commercial'),
+(124,'Carolyn','Andrea', 'Zimmerman','AB234578H' ,'1975-06-01','Litigation'),
+(128,'Jane',NULL,'Zabokritski','JX142740G','1980-03-20','Commercial'),
+(125,'Roberto',	NULL,	'Young'	,'VO928631C' 	,'1984-09-22'	,'Commercial')
+
+SELECT * FROM EmployeeTemporal
+
+UPDATE EmployeeTemporal 
+SET EmployeeLastName = 'Smith' WHERE EmployeeNumber = 124
+UPDATE EmployeeTemporal 
+SET EmployeeLastName = 'Albert' WHERE EmployeeNumber = 124
+
+DROP TABLE EmployeeTemporal
